9dee61b4afba7735c52c1e8f34a6ed0b799420dd
Chris Desktop <chrisk357@gmail.com>
2019-08-02 13:33:22 -0500

    Time Card Model Created

--- Models/TimeCard.cs
+++ Models/TimeCard.cs
@@ -8,7 +8,13 @@ namespace PayApp.Models
     public class TimeCard
     {
         public int Id { get; set; }
-        public string Location { get; set; }
+        public string LocationName { get; set; }
+        public decimal ClockInHour { get; set; }
+        public decimal ClockInMinute { get; set; }
+        public decimal PayRate { get; set; }
+        public DateTime Date { get; set; }
+        public decimal TotalHour { get; set; }
+        public decimal TotalMinute { get; set; }
 
 
     }
