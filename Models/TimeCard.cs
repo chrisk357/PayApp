03ea68a4843a8f47440d41fa8fa4511f2135aa8f
Chris Desktop <chrisk357@gmail.com>
2019-08-02 14:02:26 -0500

    Added TimeCard Context

--- Models/TimeCard.cs
+++ Models/TimeCard.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,7 +12,10 @@ namespace PayApp.Models
         public string LocationName { get; set; }
         public decimal ClockInHour { get; set; }
         public decimal ClockInMinute { get; set; }
+        public decimal ClockOutHour { get; set; }
+        public decimal ClockOutMinute { get; set; }
         public decimal PayRate { get; set; }
+        [DataType(DataType.Date)]
         public DateTime Date { get; set; }
         public decimal TotalHour { get; set; }
         public decimal TotalMinute { get; set; }
